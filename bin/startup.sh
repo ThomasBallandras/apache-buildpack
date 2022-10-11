4baca89f2ab888c1af4c90ff12f3f209ad99adc3
Thomas Ballandras <thomas.ballandras@scalingo.com>
2022-10-11 11:10:00 +0200

    changed compile

--- bin/startup.sh
+++ bin/startup.sh
@@ -3,8 +3,9 @@ set -x
 
 # Compiling erb file
 echo "compiling conf file..."
-ls -la /app/vendor/apache2/conf/
 /app/vendor/apache2/conf/httpd.conf.erb > /app/vendor/apache2/conf/httpd.conf
+
+ls -la /app/vendor/apache2/conf/
 
 # Starting
 echo "Starting Apache..."
