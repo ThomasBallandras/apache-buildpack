dbfc73eadd3810b0eb948556dec9e8bdd6fccc50
Thomas Ballandras <thomas.ballandras@scalingo.com>
2022-10-11 13:56:58 +0200

    changed compile

--- bin/startup.sh
+++ bin/startup.sh
@@ -5,12 +5,6 @@ set -x
 echo "compiling conf file..."
 erb /app/vendor/apache2/conf/httpd.conf.erb > /app/vendor/apache2/conf/httpd.conf
 
-cat /app/vendor/apache2/conf/httpd.conf
-
 # Starting
 echo "Starting Apache..."
 /app/.apt/usr/sbin/apache2 -f /app/vendor/apache2/conf/httpd.conf
-
-cat /app/vendor/apache2/logs/error_log
-
-ls /app/vendor/apache2/www/
