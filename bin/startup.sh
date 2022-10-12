00e953bbd13d555284dfd0f6f5c41e3241b48ef2
Thomas Ballandras <thomas.ballandras@scalingo.com>
2022-10-12 16:40:29 +0200

    update files

--- bin/startup.sh
+++ bin/startup.sh
@@ -4,6 +4,9 @@
 echo "compiling conf file..."
 erb $HOME/vendor/apache2/conf/httpd.conf.erb > $HOME/vendor/apache2/conf/httpd.conf
 
+#Pause for 2 sec to allow for erb command to complete
+sleep 2
+
 # Starting
 echo "Starting Apache..."
 $HOME/.apt/usr/sbin/apache2 -f $HOME/vendor/apache2/conf/httpd.conf
